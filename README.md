7fb37eba17cf159d90e5a920db5c76eead590443
arboriginal <arbo@seb.re>
2019-02-09 03:38:59 +0100

    First release

---
+++ README.md
@@ -0,0 +1,22 @@
+# SCT-CoordsTracker
+
+This is a tracker addon for [SimpleCompass](https://www.spigotmc.org/resources/simplecompass.63140/).
+It allows to track specific coordinates.
+
+## How to install
+
+- Drop the [jar file](https://github.com/arboriginal/SCT-CoordsTracker/releases) into your `plugins/SimpleCompass/trackers` folder
+- Restart your server.
+
+## Configuration
+
+Edit the file `plugins/SimpleCompass/trackers/CoordsTracker.yml` (automatically created the first time the tracker is loaded).
+
+Read [settings.yml](https://github.com/arboriginal/SCT-CoordsTracker/blob/master/src/settings.yml) to have a look on available parameters.
+
+## Permissions
+
+- To use this tracker, players must have:
+    - **scompass.use**
+    - **scompass.track**
+    - **scompass.track.COORDS** (or **scompass.track.***)
---
+++ src/me/arboriginal/SimpleCompass/trackers/CoordsTracker.java
@@ -0,0 +1,93 @@
+package me.arboriginal.SimpleCompass.trackers;
+
+import java.util.List;
+import org.bukkit.entity.Player;
+import com.google.common.collect.ImmutableMap;
+import me.arboriginal.SimpleCompass.plugin.AbstractTracker;
+import me.arboriginal.SimpleCompass.plugin.SimpleCompass;
+
+public class CoordsTracker extends AbstractTracker {
+  // ----------------------------------------------------------------------------------------------
+  // Constructor methods
+  // ----------------------------------------------------------------------------------------------
+
+  public CoordsTracker(SimpleCompass plugin) {
+    super(plugin);
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Actions methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public List<TrackingActions> getActionsAvailable(Player player, boolean keepUnavailable) {
+    List<TrackingActions> list = super.getActionsAvailable(player, keepUnavailable);
+
+    list.add(TrackingActions.ADD);
+
+    if (keepUnavailable || !list(player, null, "").isEmpty()) {
+      list.add(TrackingActions.DEL);
+      list.add(TrackingActions.START);
+      list.add(TrackingActions.STOP);
+    }
+
+    return list;
+  }
+
+  public TargetSelector requireTarget(TrackingActions action) {
+    return action.equals(TrackingActions.ADD) ? TargetSelector.NEWCOORDS : super.requireTarget(action);
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Tracker methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public String trackerID() {
+    return "COORDS";
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Command methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public boolean perform(Player player, String command, TrackingActions action, String target, String[] args) {
+    if (target == null && !action.equals(TrackingActions.ADD)) {
+      sendMessage(player, "missing_target");
+      return true;
+    }
+
+    switch (action) {
+      case ADD:
+        if (args.length < 3)
+          sendMessage(player, "missing_target");
+        else if (set(player, args[2], getCoords(player, args))) {
+          sendMessage(player, "ADD", ImmutableMap.of("target", args[2]));
+          if (settings.getBoolean("settings.auto_activated")) activate(player, args[2], false);
+        }
+        else
+          sendMessage(player, "target_exists", ImmutableMap.of("target", args[2]));
+        break;
+
+      case DEL:
+        sendMessage(player, del(player, args[2]) ? "DEL" : "target_not_found", ImmutableMap.of("target", args[2]));
+        break;
+
+      case START:
+        if (!activate(player, args[2], true)) break;
+        sendMessage(player, "START", ImmutableMap.of("target", args[2]));
+        break;
+
+      case STOP:
+        disable(player, args[2]);
+        sendMessage(player, "STOP", ImmutableMap.of("target", args[2]));
+        break;
+
+      default:
+        return false;
+    }
+
+    return true;
+  }
+}
